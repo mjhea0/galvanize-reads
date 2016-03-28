4c62eda5ea5df944c7a99c2e4d29f8201e70a558
Michael Herman <hermanmu@gmail.com>
2016-03-27 18:30:40 -0600

    added search to index

--- src/server/db/queries.js
+++ src/server/db/queries.js
@@ -79,5 +79,5 @@ module.exports = {
   addAuthor: addAuthor,
   updateAuthor: updateAuthor,
   deleteAuthor: deleteAuthor,
-  makeAdmin: makeAdmin
+  makeAdmin: makeAdmin,
 };
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+var queries = require('../db/queries');
+
 
 router.get('/', function(req, res, next) {
   res.render('index', {
     user: req.user,
     messages: req.flash('messages')
+  });
+});
+
+router.get('/results', function(req, res, next) {
+  res.render('results', {
+    user: req.user,
+    results: req.session.results,
+    messages: req.flash('messages')
+  });
+  delete req.session.results;
+});
+
+router.post('/search', function(req, res, next) {
+  var term = req.body.term;
+  queries.getBooks()
+  .then(function(results) {
+    var allResults = results.filter(function(el) {
+      return el.genre.toLowerCase() === term.toLowerCase();
+    });
+    if (allResults.length) {
+      req.session.results = allResults;
+      res.redirect('/results');
+    } else {
+      req.flash('messages', {
+        status: 'danger',
+        value: 'No results. Please try again.'
+      });
+      res.redirect('/');
+    }
+  })
+  .catch(function(err) {
+    return next(err);
   });
 });
 
